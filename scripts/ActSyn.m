b87e96fddef16ae328c8716699cf7d9d250e3387
Tea Tompoš <teatompos@ip-145-137-153-124.wlan-int.ru.nl>
2024-01-11 17:52:55 +0100

    add actsyn

---
+++ scripts/ActSyn.m
@@ -0,0 +1,48 @@
+function SynActivity=ActSyn(synapse,iter)
+    % activity arrays for 21 connections in the network stored in structure s
+    % 1 = active
+    % 0 = inactive
+
+    % active synapse means it is actively varied depending on the scaling factor
+
+    if iter==0 % variable iter is an integer, but when is set to 'vary', the synapse will be varied for sure (otherwise depends on the iteration)
+        SynActivity=0;
+    else
+        % changes in intralaminar projections
+        s.l2ee=   [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0];
+        s.l2ei=   [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0];
+        s.l2ie=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0];
+        s.l2ii=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1];
+
+        s.l3ee=   [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0];
+        s.l3ei=   [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0];
+        s.l3ie=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0];
+        s.l3ii=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0];
+
+        s.l4ee=   [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0];
+        s.l4ei=   [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0];
+        s.l4ie=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0];
+        s.l4ii=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0];
+
+        % changes in translaminar projections
+        s.thl4e=  [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.thl4i=  [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        s.l4el3e= [0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.l4el3i= [0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.l4il3e= [0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        s.l3el2e= [0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.l3el2i= [0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.l3il2e= [0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        s.l3il2i= [0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        try 
+            pos=getfield(s,synapse);
+            SynActivity=pos(iter);
+        catch ME
+            disp('Either the synapse which you want to vary does not exist in the structure or the iteration number is too large (so there is no information on whether the synapse should be varied).')
+            SynActivity = 0;
+        end
+    end
+end
